96b90c88eb7bef213b6ec0d0f561774c5fd2e6dd
zhouzigui <zhouzigui@elex-tech.com>
2019-08-23 13:51:12 +0800

    engine

---
+++ GEngine/syncToClient.bat
@@ -0,0 +1,4 @@
+xcopy bin\Debug\netcoreapp2.2\SaveManager.dll F:\Works\GameBundles\Assets\plugins  /C /F /H /R /Y
+xcopy bin\Debug\netcoreapp2.2\SaveManager.pdb F:\Works\GameBundles\Assets\plugins  /C /F /H /R /Y
+xcopy bin\Debug\netcoreapp2.2\UnityEngine.dll F:\Works\GameBundles\Assets\plugins  /C /F /H /R /Y
+pause
